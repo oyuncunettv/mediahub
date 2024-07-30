7a4065aa8e9cc9728815279789065d6a9ddf4e07
oyuncunettv <121956585+oyuncunettv@users.noreply.github.com>
2024-07-30 19:56:21 +0300

    Update iptv.js

--- iptv.js
+++ iptv.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const nsfwToggle = document.getElementById('nsfwToggle');
     const videoPlayer = document.getElementById('iptvPlayer');
     const videoSource = document.getElementById('videoSource');
     const prevButton = document.getElementById('prevVideo');
@@ -8,56 +7,38 @@ document.addEventListener('DOMContentLoaded', function() {
     let videos = [];
     let currentIndex = 0;
 
+    // Fetch video list from JSON file
     fetch('videolist.json')
         .then(response => response.json())
         .then(data => {
-            videos = nsfwToggle.checked ? data.nsfwVideos : data.defaultVideos;
-            videos = shuffleArray(videos);
-            updateVideoSource();
+            videos = data.videos;
+            if (videos.length > 0) {
+                loadVideo();
+            }
         })
         .catch(error => console.error('Error loading video list:', error));
 
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
-
-    function updateVideoSource() {
+    function loadVideo() {
         if (videos.length > 0) {
             videoSource.src = videos[currentIndex];
             videoPlayer.load();
         }
     }
 
-    function showPreviousVideo() {
+    function showPrevVideo() {
         if (videos.length > 0) {
             currentIndex = (currentIndex - 1 + videos.length) % videos.length;
-            updateVideoSource();
+            loadVideo();
         }
     }
 
     function showNextVideo() {
         if (videos.length > 0) {
             currentIndex = (currentIndex + 1) % videos.length;
-            updateVideoSource();
+            loadVideo();
         }
     }
 
-    nsfwToggle.addEventListener('change', function() {
-        fetch('videolist.json')
-            .then(response => response.json())
-            .then(data => {
-                videos = nsfwToggle.checked ? data.nsfwVideos : data.defaultVideos;
-                videos = shuffleArray(videos);
-                currentIndex = 0; // Reset index
-                updateVideoSource();
-            })
-            .catch(error => console.error('Error loading video list:', error));
-    });
-
-    prevButton.addEventListener('click', showPreviousVideo);
+    prevButton.addEventListener('click', showPrevVideo);
     nextButton.addEventListener('click', showNextVideo);
 });
